d44864700be6aa510d4827aa3f28e56252f8743e
Zeel Chotaliya <134128833+ZeelVC@users.noreply.github.com>
2026-09-09 23:47:42 +0530

    fix: export metric scaling and fusion helpers

--- src/floorplan_ai/depth/__init__.py
+++ src/floorplan_ai/depth/__init__.py
@@ -1,8 +1,26 @@
 from .base import MetricDepthEstimator, MetricDepthResult
 from .depth_pro import DepthProEstimator
-from .fusion import RobustScaleResult, robust_scale, robust_scale_estimate, transform_points, unproject_depth
+from .fusion import (
+    RobustScaleResult,
+    robust_scale,
+    robust_scale_estimate,
+    transform_points,
+    unproject_depth,
+    scale_points,
+    scale_pose_translation,
+    fuse_metric_clouds,
+)
 
 __all__ = [
-    "MetricDepthEstimator", "MetricDepthResult", "DepthProEstimator", "RobustScaleResult",
-    "robust_scale", "robust_scale_estimate", "transform_points", "unproject_depth",
+    "MetricDepthEstimator",
+    "MetricDepthResult",
+    "DepthProEstimator",
+    "RobustScaleResult",
+    "robust_scale",
+    "robust_scale_estimate",
+    "transform_points",
+    "unproject_depth",
+    "scale_points",
+    "scale_pose_translation",
+    "fuse_metric_clouds",
 ]
