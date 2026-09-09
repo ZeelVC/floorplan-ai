e4472a3bd8e8c177829d98604fa16749f8bbcc8d
Zeel Chotaliya <134128833+ZeelVC@users.noreply.github.com>
2026-09-10 00:26:28 +0530

    feat: expose global property optimization

--- src/floorplan_ai/stitching/__init__.py
+++ src/floorplan_ai/stitching/__init__.py
@@ -1,4 +1,16 @@
 from .registration import RegistrationResult, register_points
 from .room_matching import candidate_pairs
 from .graph import components
+from .optimization import build_property_topology, optimize_property
 from .reconciliation import reconcile, reconcile_models
+
+__all__ = [
+    "RegistrationResult",
+    "register_points",
+    "candidate_pairs",
+    "components",
+    "build_property_topology",
+    "optimize_property",
+    "reconcile",
+    "reconcile_models",
+]
