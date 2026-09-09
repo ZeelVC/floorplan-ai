d645acf960d833dec6fdb6bd38ad06b968ca569a
Zeel Chotaliya <134128833+ZeelVC@users.noreply.github.com>
2026-09-10 00:18:48 +0530

    Export multi-model stitching reconciler

--- src/floorplan_ai/stitching/__init__.py
+++ src/floorplan_ai/stitching/__init__.py
@@ -1,4 +1,4 @@
 from .registration import RegistrationResult, register_points
 from .room_matching import candidate_pairs
 from .graph import components
-from .reconciliation import reconcile
+from .reconciliation import reconcile, reconcile_models
